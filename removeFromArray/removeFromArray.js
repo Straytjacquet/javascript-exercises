320a2dff38bc1034f2dc077721a3ce65b39ee36b
Straytjacquet <straichen@gmail.com>
2020-01-25 15:13:22 -0800

    Solutions to coding problems

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(array, arg1, arg2, arg3, arg4) {
+
+ if (array.indexOf(arg1) > -1) {
+let firstChange= array.splice(array.indexOf(arg1) , 1);
+}
+
+ if (array.indexOf(arg2) > -1) {
+var secondChange= array.splice(array.indexOf(arg2) , 1);
+}
+ if (array.indexOf(arg3) > -1) {
+let thirdChange= array.splice(array.indexOf(arg3) , 1);
+}
+ if (array.indexOf(arg4) > -1) {
+let fourthChange= array.splice(array.indexOf(arg4) , 1);
+}
+
+return array;
 }
 
 module.exports = removeFromArray
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(word , num) {
+  if (num < 0) {
+    return 'ERROR'
+  } else {
+    var repeat = word.repeat([num]);
+    return repeat;
+  if (num > 2) {
+    return 'Hello Poodle!'
+  } else {
+    var repeat = word.repeat ([num]);
+    return repeat
+  }
+}
 }
 
 module.exports = repeatString
